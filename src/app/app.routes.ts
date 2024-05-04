67abaf91028d232c62ce1560e9b42ec496cecd4c
ammadjaved <ammadjaved123@gmail.com>
2024-05-04 12:41:04 +0500

    basic structure of angular

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/signup', pathMatch: 'full' },
+    { path: 'login', loadComponent: () => import('./login/login.component').then(m => m.LoginComponent) },
+    { path: 'signup', loadComponent: () => import('./signup/signup.component').then(m => m.SignupComponent) }
+];
