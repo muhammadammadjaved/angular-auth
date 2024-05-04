c5188fd7f58d6ff87a948c1b345e549dcd1afc08
ammadjaved <ammadjaved123@gmail.com>
2024-05-05 01:27:13 +0500

    formating signup

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,45 +1,47 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../auth.service';
 // declare var google:any;
 
 @Component({
   selector: 'app-signup',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
 })
 export class SignupComponent {
-  
-  signupForm!: FormGroup    ;
+  signupForm!: FormGroup;
   errorMessage: string = '';
 
-  constructor(private fb: FormBuilder,private service: AuthService) {}
+  constructor(private fb: FormBuilder, private service: AuthService) {}
 
   ngOnInit() {
-
     this.signupForm = this.fb.group({
       fullName: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
-      password: ['', Validators.required]
+      password: ['', Validators.required],
     });
   }
 
   onSubmit() {
-    this.service.post('api/signup',this.signupForm.value).subscribe(
-      (response:any) => {
+    this.service.post('api/signup', this.signupForm.value).subscribe(
+      (response: any) => {
         console.log(response);
       },
-      (error:any) => {
+      (error: any) => {
         // Handle error if needed
         console.error(error);
       }
     );
   }
 
-  googleSignup(){
-  }
-
+  googleSignup() {}
 }
